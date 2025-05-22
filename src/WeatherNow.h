7d7225ab6b14d99eaa3d3a4e1d09373f2f63dbcc
abackup <40161397+abackup@users.noreply.github.com>
2025-05-22 17:19:16 +0800

    Update WeatherNow.h

--- src/WeatherNow.h
+++ src/WeatherNow.h
@@ -28,7 +28,7 @@ class WeatherNow {
     float getPrecip();
 
   private:
-    const char* _host = "devapi.heweather.net"; // 服务器地址
+    const char* _host = "n949vgkkgg.re.qweatherapi.com"; // 服务器地址
     const int httpsPort = 443;
 
     String _requserKey;  // 私钥
