6179c87076bc69435a5abcab80d1b854a77c25db
abackup <40161397+abackup@users.noreply.github.com>
2025-05-22 21:21:04 +0800

    Update WeatherNow.ino

--- example/WeatherNow/WeatherNow.ino
+++ example/WeatherNow/WeatherNow.ino
@@ -1,49 +1,69 @@
 #include <ESP32_Heweather.h>
 
-const char* ssid     = "SSID";     // WiFi名称
-const char* password = "PASSWORD"; // WiFi密码
+const char *ssid = "SSID";     // WiFi名称
+const char *passwd = "PASSWORD"; // WiFi密码
 
-String UserKey = "YOUR-KEY";   // 私钥 https://dev.heweather.com/docs/start/get-api-key
-String Location = "101010100"; // 城市代码 https://github.com/heweather/LocationList,表中的 Location_ID
-String Unit = "m";             // 公制-m/英制-i
-String Lang = "zh";            // 语言 英文-en/中文-zh
-float ROUND = 1.5;             // 更新间隔<分钟>实时天气API 10~20分钟更新一次
+String Token = "******c21f7c4e63a03dcb2ec0af98bc"; // api-key
+String Location = "1011901**";                     //城市id
+String API_Host = "******kkgg.re.qweatherapi.com";   //API_Host
+String Unit = "m";  // 公制-m/英制-i
+String Lang = "en"; // 语言 英文-en/中文-zh
+float ROUND = 1.5;  // 更新间隔<分钟>实时天气API 10~20分钟更新一次
 
 WeatherNow weatherNow;
 
-void setup(){
-  Serial.begin(9600);
+void setup()
+{
+  Serial.begin(115200);
   Serial.println("");
 
-  ConnectWiFi(); // 连接WiFi
-  weatherNow.config(UserKey, Location, Unit, Lang); // 配置请求信息
+  ConnectWiFi();                                            // 连接WiFi
+  weatherNow.config(API_Host, Token, Location, Unit, Lang); // 配置请求信息
 }
 
-void loop(){
-  if(weatherNow.get()){ // 获取天气更新
+void loop()
+{
+  if (weatherNow.get())
+  { // 获取天气更新
     Serial.println(F("======Weahter Now Info======"));
     Serial.print("Server Response: ");
-    Serial.println(weatherNow.getServerCode());  // 获取API状态码
+    Serial.println(weatherNow.getServerCode()); // 获取API状态码
     Serial.print(F("Last Update: "));
-    Serial.println(weatherNow.getLastUpdate());  // 获取服务器更新天气信息时间
+    Serial.println(weatherNow.getLastUpdate()); // 获取服务器更新天气信息时间
     Serial.print(F("Temperature: "));
-    Serial.println(weatherNow.getTemp());        // 获取实况温度
+    Serial.println(weatherNow.getTemp()); // 获取实况温度
     Serial.print(F("FeelsLike: "));
-    Serial.println(weatherNow.getFeelLike());    // 获取实况体感温度
+    Serial.println(weatherNow.getFeelLike()); // 获取实况体感温度
     Serial.print(F("Icon: "));
-    Serial.println(weatherNow.getIcon());        // 获取当前天气图标代码
+    Serial.println(weatherNow.getIcon()); // 获取当前天气图标代码
     Serial.print(F("Weather Now: "));
     Serial.println(weatherNow.getWeatherText()); // 获取实况天气状况的文字描述
     Serial.print(F("windDir: "));
-    Serial.println(weatherNow.getWindDir());     // 获取实况风向
+    Serial.println(weatherNow.getWindDir()); // 获取实况风向
     Serial.print(F("WindScale: "));
-    Serial.println(weatherNow.getWindScale());   // 获取实况风力等级
+    Serial.println(weatherNow.getWindScale()); // 获取实况风力等级
     Serial.print(F("Humidity: "));
-    Serial.println(weatherNow.getHumidity());    // 获取实况相对湿度百分比数值
+    Serial.println(weatherNow.getHumidity()); // 获取实况相对湿度百分比数值
     Serial.print(F("Precip: "));
-    Serial.println(weatherNow.getPrecip());      // 获取实况降水量,毫米
+    Serial.println(weatherNow.getPrecip()); // 获取实况降水量,毫米
+
+    Serial.print(F("Pressure: "));
+    Serial.println(weatherNow.getPressure()); // 获取实况风力等级
+    Serial.print(F("Vis: "));
+    Serial.println(weatherNow.getVis()); // 获取实况风力等级
+    Serial.print(F("Cloud: "));
+    Serial.println(weatherNow.getCloud()); // 获取实况风力等级
+    Serial.print(F("Dew: "));
+    Serial.println(weatherNow.getDew()); // 获取实况风力等级
+    Serial.print(F("Wind360: "));
+    Serial.println(weatherNow.getWind360()); // 获取实况风力等级
+    Serial.print(F("WindSpeed: "));
+    Serial.println(weatherNow.getWindSpeed()); // 获取实况风力等级
+
     Serial.println(F("========================"));
-  } else {    // 更新失败
+  }
+  else
+  { // 更新失败
     Serial.println("Update Failed...");
     Serial.print("Server Response: ");
     Serial.println(weatherNow.getServerCode()); // 参考 https://dev.heweather.com/docs/start/status-code
@@ -52,21 +72,26 @@ void loop(){
 }
 
 // 连接WiFi
-void ConnectWiFi(){
+void ConnectWiFi()
+{
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, passwd);
   Serial.print("Connecting to ");
   Serial.print(ssid);
 
   int i = 0;
-  while (WiFi.status() != WL_CONNECTED && (i < 10)) {
+  while (WiFi.status() != WL_CONNECTED && (i < 10))
+  {
     delay(1000);
     Serial.print('.');
     i++;
   }
-  if (i == 10) { // 10s失败
+  if (i == 10)
+  { // 10s失败
     Serial.println("WiFi Connection Failed");
-  } else { // 成功
+  }
+  else
+  { // 成功
     Serial.println("");
     Serial.println("WiFi Connection Successful!");
     Serial.print("IP address:   ");
