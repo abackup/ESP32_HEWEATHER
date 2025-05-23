ea0861f973e68b74422d6dfba915e928670e676b
abackup <40161397+abackup@users.noreply.github.com>
2025-05-23 13:35:21 +0800

    Update AirQuality.h

--- src/AirQuality.h
+++ src/AirQuality.h
@@ -5,39 +5,51 @@
 #include <ArduinoJson.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
-//#include <WiFiClientSecureBearSSL.h>
+// #include <WiFiClientSecureBearSSL.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoUZlib.h>
-// #define DEBUG   // 调试用
+#pragma once
+// #define DEBUG // 调试用
 
-class AirQuality {
-  public:
-    AirQuality();
+class AirQuality
+{
+public:
+  AirQuality();
 
-    void config(String userKey, String location, String unit, String lang);
-    bool get();
-    String getLastUpdate();
-    String getServerCode();
-    int getAqi();
-    String getCategory();
-    String getPrimary();
+  void config(String API_Host, String Token, String location, String lang);
+  bool get();
+  String getServerCode();
+  String getLastUpdate();
+  int getAqi();
+  String getCategory();
+  int getPm10();
+  int getPm2p5();
+  int getNo2();
+  int getSo2();
+  float getCo();
+  int getO3();
 
-  private:
-    const char* _host = "devapi.heweather.net"; // 服务器地址
-    const int httpsPort = 443;
+private:
+  String _reqToken;    // 私钥
+  String _reqLocation; // 位置
+  String _reqHost;
+  String _reqLang; // 语言
 
-    String _requserKey;  // 私钥
-    String _reqLocation; // 位置
-    String _reqUnit;     // 单位
-    String _reqLang;     // 语言
+  const char *_host = _reqHost.c_str(); // 服务器地址
+  const int httpsPort = 443;
 
-    void _parseNowJson(String payload);
+  void _parseNowJson(String payload); // 解析json信息
 
-    String _response_code =  "no_init";   // API状态码
-    String _last_update_str = "no_init";  // 当前API最近更新时间
-    int _now_aqi_int = 999;               // 实时空气质量指数
-    String _now_category_str = "no_init"; // 实时空气质量指数级别
-    String _now_primary_str = "no_init";  // 实时空气质量的主要污染物，优时返回 NA
-
+  String _response_code = "no_init";   // API状态码
+  String _last_update_str = "no_init"; // 当前API最近更新时间
+  int _now_aqi_int = 999;             // 实况温度
+  String _now_category_str = "no_init";    // 实况空气质量类别
+  int _now_pm10_int = 999;              // 实况pm10
+  int _now_pm2p5_int = 999;            // 实况pm2.5
+  int _now_no2_int = 999;              // 实况no2
+  int _now_so2_int = 999;        // 实况so2
+  float _now_co_float = 999;     // 实况co
+  int _now_o3_int = 999;          // 实况o3
 };
+
 #endif
